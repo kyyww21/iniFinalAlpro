2b612b7856a4bdf93344a7d327823e91df9701ac
naurafarah-sketch <naurafarah@student.upi.edu>
2025-12-18 22:26:58 +0700

    hapus data

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
-#include <stdio.h>
+void hapusData() {
+    char id[20];
+    int ditemukan = 0;
+
+    if (totalBuku == 0) {
+        printf("Data kosong, tidak ada yang dihapus.\n");
+        return;
+    }
+
+    printf("Masukkan Kode Buku yang ingin dihapus: ");
+    scanf(" %[^\n]", id);
+
+    for (int i = 0; i < totalBuku; i++) {
+        if (strcmp(perpustakaan[i].Kode, id) == 0) {
+            ditemukan = 1;
+
+            
+            for (int j = i; j < totalBuku - 1; j++) {
+                perpustakaan[j] = perpustakaan[j + 1];
+            }
+
+            totalBuku--;
+            printf("Data berhasil dihapus!\n");
+            break;
+        }
+    }
+
+    if (!ditemukan) {
+        printf("Data tidak ditemukan!\n");
+    }
+}
