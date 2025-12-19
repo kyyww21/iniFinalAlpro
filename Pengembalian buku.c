11c7c6010d266e0320dc8b412a1f2a2ccdb3675d
Khairul-arch <khairulabdurrahim98@student.upi.edu>
2025-12-20 01:06:04 +0700

    Perbaikan

--- Pengembalian buku.c
+++ Pengembalian buku.c
@@ -15,7 +15,7 @@ void pengembalianBuku() {
 
     printf("Masukkan Kode Buku : ");
     scanf(" %[^\n]", kode);
-    // Cari data peminjaman yang sesuai
+
     for (int i = 0; i < totalPeminjaman; i++) {
         if (strcmp(daftarPeminjaman[i].NamaPeminjam, nama) == 0 &&
             strcmp(daftarPeminjaman[i].KodeBuku, kode) == 0 &&
@@ -37,36 +37,32 @@ void pengembalianBuku() {
                 printf("\n-- Jumlah pengembalian melebihi jumlah pinjaman! --\n");
                 printf("Jumlah yang dipinjam hanya %d buku\n", daftarPeminjaman[i].jumlahPinjam);
             } else {
-                // Update stok buku di perpustakaan
+
                 for (int j = 0; j < totalBuku; j++) {
                     if (strcmp(perpustakaan[j].Kode, kode) == 0) {
-                        // UPDATE: Kurangi status sedangDipinjam
+
                         perpustakaan[j].sedangDipinjam -= jumlahKembali;
                         break;
                     }
                 }
 
-                // Update jumlah pinjaman di struct Peminjaman
                 daftarPeminjaman[i].jumlahPinjam -= jumlahKembali;
 
                 printf("\n-- Pengembalian Berhasil! --\n");
 
-                // Jika semua buku sudah dikembalikan, hapus data peminjaman
                 if (daftarPeminjaman[i].jumlahPinjam == 0) {
                     printf("\n-- Semua buku telah dikembalikan! Data peminjaman dihapus. --\n");
 
-                    // Geser data peminjaman
                     for (int k = i; k < totalPeminjaman - 1; k++) {
                         daftarPeminjaman[k] = daftarPeminjaman[k + 1];
                     }
                     totalPeminjaman--;
-                    // Mundurkan index i karena array bergeser, agar tidak skip data
                     i--;
                 } else {
                     printf("Sisa Pinjaman : %d buku\n", daftarPeminjaman[i].jumlahPinjam);
                 }
             }
-            break; // Keluar loop setelah ketemu
+            break;
         }
     }
 
