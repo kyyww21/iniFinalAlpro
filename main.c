ac70893fb9500d2a1c0a7e08780d56f48524016a
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-19 17:13:10 +0700

    coba sorting buku

--- main.c
+++ main.c
@@ -1,77 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+void bubbleSortBuku() {
+    struct Buku temp;
 
-struct Buku {
-    char Judul[50];
-    char Kode[20];
-    int jumlahTotal;      
-    int sedangDipinjam; 
-};
-
-struct Peminjaman {
-    char NamaPeminjam[50];
-    char Kelas[10];
-    char KodeBuku[20];
-    char JudulBuku[50];
-    int jumlahPinjam;
-};
-
-struct Buku perpustakaan[100];
-struct Peminjaman daftarPeminjaman[200];
-int totalBuku = 0;
-int totalPeminjaman = 0;
-
-
-void penambahanBuku() {
-    char kode[20];
-    int jumlahTambah;
-    int ditemukan = 0;
-
-    printf("\n--- Tambah Buku ---\n");
-    printf("Masukkan Kode Buku  : ");
-    scanf(" %[^\n]", kode);
-
-    
-    for (int i = 0; i < totalBuku; i++) {
-        if (strcmp(perpustakaan[i].Kode, kode) == 0) {
-            ditemukan = 1;
-
-            printf("\n-- Buku ditemukan! --\n");
-            printf("Judul: %s\n", perpustakaan[i].Judul);
-            printf("Stok Total: %d\n", perpustakaan[i].jumlahTotal);
-            printf("\nMasukkan jumlah yang ingin ditambahkan : ");
-            scanf("%d", &jumlahTambah);
-
-            perpustakaan[i].jumlahTotal += jumlahTambah;
-
-            printf("\n-- Stok berhasil ditambahkan! --\n");
-            printf("Stok baru: %d\n", perpustakaan[i].jumlahTotal);
-            break;
+    for (int i = 0; i < totalBuku - 1; i++) {
+        for (int j = 0; j < totalBuku - i - 1; j++) {
+            if (strcmp(perpustakaan[j].Judul, perpustakaan[j+1].Judul) > 0) {
+                temp = perpustakaan[j];
+                perpustakaan[j] = perpustakaan[j+1];
+                perpustakaan[j+1] = temp;
+            }
         }
-    }
-
-    
-    if (!ditemukan) {
-        if (totalBuku >= 100) {
-            printf("Penyimpanan penuh!\n");
-            return;
-        }
-
-        printf("\n-- Buku baru, silakan lengkapi data --\n");
-
-        strcpy(perpustakaan[totalBuku].Kode, kode);
-
-        printf("Masukkan Judul Buku : ");
-        scanf(" %[^\n]", perpustakaan[totalBuku].Judul);
-
-        printf("Masukkan Jumlah     : ");
-        scanf(" %d", &perpustakaan[totalBuku].jumlahTotal);
-
-        
-        perpustakaan[totalBuku].sedangDipinjam = 0;
-
-        totalBuku++;
-        printf("\n-- Buku baru berhasil ditambahkan --\n");
     }
 }
