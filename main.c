1971dd65254f257e2b5dbd28a9f9c63de0da8d0a
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-20 14:24:44 +0700

    file dari jovan

--- main.c
+++ main.c
@@ -253,6 +253,27 @@ void daftarBuku() {
     printf("==================================================================================================\n");
 }
 
+void daftarPeminjam(){
+
+    if (totalPeminjaman == 0){
+        printf("\n-- Belum ada data peminjaman --\n");
+        return;
+    }
+
+    printf("\n=== DAFTAR PEMINJAM BUKU ===\n");
+    printf("%-20s %-10s %-30s %-10s\n", "Nama Peminjam", "Kelas", "Judul Buku", "Jml Pinjam");
+    printf("------------------------------------------------------------------------------\n");
+
+    for (int i = 0; i < totalPeminjaman; i++) {
+        printf("%-20s %-10s %-30s %-10d\n",
+               daftarPeminjaman[i].NamaPeminjam,
+               daftarPeminjaman[i].Kelas,
+               daftarPeminjaman[i].JudulBuku,
+               daftarPeminjaman[i].jumlahPinjam);
+    }
+    printf("==============================================================================\n");
+}
+
 void hapusData() {
     char id[20];
     int ditemukan = 0;
