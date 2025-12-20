8a152fe1a26b822437e5b25920cfa6271a0ec0e0
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-20 14:26:56 +0700

    file dari sinta

--- main.c
+++ main.c
@@ -228,6 +228,20 @@ void bubbleSortBuku() {
     }
 }
 
+void sortingNamaPeminjam() {
+    struct Buku temp;
+
+    for (int i = 0; i < totalPeminjaman - 1; i++) {
+        for (int j = 0; j < totalPeminjaman - i - 1; j++) {
+            if (strcmp(daftarPeminjaman[j].NamaPeminjam, daftarPeminjaman[j+1].NamaPeminjam) > 0) {
+                temp = daftarPeminjaman[j];
+                daftarPeminjaman[j] = daftarPeminjaman[j+1];
+                daftarPeminjaman[j+1] = temp;
+            }
+        }
+    }
+}
+
 void daftarBuku() {
     if (totalBuku == 0) {
         printf("\n-- Belum ada buku di perpustakaan --\n");
